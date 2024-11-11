4efd3c6734c9741c6696645dd9f806fe346b2b6b
Olooce <asirastephen9@gmail.com>
2024-11-11 19:35:54 +0300

    Initial commit

---
+++ src/main/java/co/ke/CoreNexus/db_utils/Seeder.java
@@ -0,0 +1,14 @@
+package co.ke.CoreNexus.db_utils;
+
+/**
+ * mock_data_seedar (co.ke.CoreNexus.db_utils)
+ * Created by: oloo
+ * On: 11/11/2024. 19:27
+ * Description:
+ **/
+
+public class Seeder {
+    public static void main(String[] args) {
+
+    }
+}
